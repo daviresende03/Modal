1bcd956b73c877ed5d95f82a92542636973e09ef
daviresende03 <daviresendedev@gmail.com>
2022-12-13 08:13:10 -0300

    ModalCreate

---
+++ Modal/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modal.Models;
+using System.Diagnostics;
+
+namespace Modal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Modal/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    var PlaceHolderElement = $('#PlaceHolderHere');
+    $('a[data-toggle = "ajax-modal"]').click(function (event) {
+        var url = $(this).data('url');
+        var decodeUrl = decodeURIComponent(url);
+        $.get(decodeUrl).done(function (data) {
+            PlaceHolderElement.html(data);
+            PlaceHolderElement.find('.modal').modal('show');
+        })
+    })
+})
